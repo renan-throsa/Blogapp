253c00bf071a76179632a3853327bd0e07d9c8e8
Renan Rosa <renannojosa@gmail.com>
2021-08-22 19:35:16 -0300

    Adding user feature implemented.

--- Startup.cs
+++ Startup.cs
@@ -18,8 +18,7 @@ namespace Blogapp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
-            services.AddAutoMapper(typeof(Startup));
+            services.AddRazorPages();            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
